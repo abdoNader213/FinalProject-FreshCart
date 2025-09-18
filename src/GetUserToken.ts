700630369e06c71fa1d53b2a79979901ff02d260
abdoNader213 <bodanader091@gmail.com>
2025-09-18 23:03:28 +0300

    commir three

--- src/GetUserToken.ts
+++ src/GetUserToken.ts
@@ -1,25 +1,20 @@
 'use server'
 
-import { decode, JWT } from "next-auth/jwt"
+import { decode } from "next-auth/jwt"
 import { cookies } from "next/headers"
 
-interface TokenData {
-  token?: string
-  [key: string]: unknown
-}
 
-export async function getUserToken(): Promise<string | null> {
-  const TokenSession = (process.env.NODE_ENV === "production"?'__Secure-next-auth.callback-url':"next-auth.session-token")
+export async function getUserToken() {
+  const TokenSession = (process.env.NODE_ENV === "production"? '__Secure-next-auth.callback-url':"next-auth.session-token")
   const cookiesData = await cookies()
   const encryptToken = cookiesData.get(TokenSession)?.value
 
-  if (!encryptToken) return null
+
 
   const data = await decode({
     token: encryptToken,
-    secret: process.env.NEXTAUTH_SECRET!,
-  }) as TokenData | null
+    secret: process.env.NEXTAUTH_SECRET!})
 
-  return data?.token || null
+  return data?.token
 }
 
