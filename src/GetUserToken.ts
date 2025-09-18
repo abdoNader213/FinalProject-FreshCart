9daa59e76c646d07d3932459352d5987b547b5f4
abdoNader213 <bodanader091@gmail.com>
2025-09-18 23:07:27 +0300

    commit four

--- src/GetUserToken.ts
+++ src/GetUserToken.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers"
 
 
 export async function getUserToken() {
-  const TokenSession = (process.env.NODE_ENV === "production"? '__Secure-next-auth.callback-url':"next-auth.session-token")
+  const TokenSession = (process.env.NODE_ENV === "production"? '__Secure-next-auth.session-token':"next-auth.session-token")
   const cookiesData = await cookies()
   const encryptToken = cookiesData.get(TokenSession)?.value
 
